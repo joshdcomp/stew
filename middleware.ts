09910fcef85462b3012fec3ce26e109e16ce3d67
Josh Compton <joshdcomp@gmail.com>
2024-11-15 11:17:38 -0500

    fix: screw auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth"
-import { baseAuthConfig } from '@/auth.config'
+// import NextAuth from "next-auth"
+// import { baseAuthConfig } from '@/auth.config'
 
 // prisma doesn't play nicely on edge fn's so auth on middleware is a standalone instance
-export const { auth: middleware } = NextAuth(baseAuthConfig)
- 
+// export const { auth: middleware } = NextAuth(baseAuthConfig)
+export default async () => {}
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
